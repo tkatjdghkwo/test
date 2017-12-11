285ab612dcb222b4fec7fd2d8b52cd6b8dce68a4
nicehorang@google.com <nicehorang@google.com>
2017-12-11 15:26:45 +0900

    마스터 작업중 출력

--- gitTest/src/gitTest/HellowWorld.java
+++ gitTest/src/gitTest/HellowWorld.java
@@ -4,6 +4,6 @@ public class HellowWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!!!!");
 		System.out.println("안녕하세요");
-		
+		System.out.println("마스터 작업중");
 	}
 }
